65e53ef0ba9a2f5eb0c277520fbdbb40a4ae513d
drubdom608 <153735799+drubdom608@users.noreply.github.com>
2024-04-10 13:39:34 +0200

    ndsoivyhuafo fbud

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    basic.pause(1000)
-    maqueen.motorStop(maqueen.Motors.M1)
-    basic.pause(1000)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+    basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 })
